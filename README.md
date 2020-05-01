cfe4c70d7ecc258a316cf135d2b245395d7b1a33
uyennguyen16900 <uyennguyen16900@gmail.com>
2020-05-01 14:44:45 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Tic-Tac-Toe AI
+
+XOX is an unbeatable Tic-Tac-Toe game with a reliable Artificial Intelligence using the Minimax algorithm. It will choose the move with maximum score when it is the AI's turn and choose the move with the minimum score when it is the human player's turn. Using this strategy, Minimax avoids losing to the human player.
+
+### Minimax Algorithm
+
+Minimax is used in decision making and game theory to find the optimal move for a player assuming that the other player is also playing optimally. It's widely used in two player turn-based games such as Tic-Tac-Toe, Chess, Mancala, Go, etc.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Installing
+
+Clone the repository
+```
+git clone https://github.com/uyennguyen16900/tic-tac-toe-AI.git
+```
+
+Go to the repo folder
+```
+cd tic-tac-toe-AI
+```
+
+Run the python version of the game
+
+```
+python3 tictactoe.py
+```
+
+## Acknowledgments
+
+* Inspiration: https://thecodingtrain.com/CodingChallenges/154-tic-tac-toe-minimax.html
---
+++ tictactoe.py
@@ -0,0 +1,167 @@
+import math
+
+# X is computer, O is human player
+players = ['X', 'O']
+
+def play_move(state, player, input_place):
+    """Let player place their mark on the board"""
+    while True:
+        try:
+            row, column = input_place.split(" ")
+            break
+        except ValueError:
+            input_place = input("Please enter valid inputs (e.g 1 3, 2 2): ")
+
+    if state[int(row)][int(column)] == " ":
+        state[int(row)][int(column)] = player
+    else:
+        input_place = input("The position has already been occupied! Choose again: ")
+        play_move(state, player, input_place)
+
+def empty_cells(state):
+    """"""
+    cells = []
+    for r, cell_row in enumerate(state):
+        for c, cell in enumerate(cell_row):
+            if cell == " ":
+                cells.append([r, c])
+
+    return cells
+
+def evaluate(state, depth, winner):
+    """Return 1 if computer wins; -1 if human wins; 0 if draw"""
+    if state == "Done" and winner == "X":
+        return 10 - depth
+    elif state == "Done" and winner == "O":
+        return -10 + depth
+    elif state == "Draw":
+        return 0
+
+def minimax(state, depth, player):
+    """"""
+    winner, current_state = check_current_state(state)
+    if current_state == "Draw" or current_state == "Done":
+        return {"score": evaluate(current_state, depth, winner), "move": "-1 -1"}
+
+    best = {}
+
+    if player == 'X':
+        best["score"] = -math.inf
+        best['move'] = ""
+    else:
+        best["score"] = math.inf
+        best['move'] = ""
+
+    depth =+ 1
+
+    for cell in empty_cells(state):
+        x, y = cell[0], cell[1]
+        state[x][y] = player
+        if player == "O":
+            result = minimax(state, depth, "X")
+        else:
+            result = minimax(state, depth, "O")
+        state[x][y] = " "
+        result['move'] = "{} {}".format(x, y)
+
+        # if player is computer
+        if player == 'X':
+            if result['score'] > best['score']:
+                best = result
+        # if player is human
+        else:
+            if result['score'] < best['score']:
+                best = result
+
+    return best
+
+
+def display_board(state):
+    """"""
+    print("        0     1     2  ")
+    print("     -------------------")
+    print("  0  |  {}  |  {}  |  {}  |".format(state[0][0], state[0][1], state[0][2]))
+    print("     -------------------")
+    print("  1  |  {}  |  {}  |  {}  |".format(state[1][0], state[1][1], state[1][2]))
+    print("     -------------------")
+    print("  2  |  {}  |  {}  |  {}  |".format(state[2][0], state[2][1], state[2][2]))
+    print("     -------------------")
+
+
+def check_current_state(state):
+    """"""
+    win_state = [[state[0][0], state[0][1], state[0][2]],
+                 [state[0][0], state[1][0], state[2][0]],
+                 [state[1][0], state[1][1], state[1][2]],
+                 [state[2][0], state[2][1], state[2][2]],
+                 [state[0][1], state[1][1], state[2][1]],
+                 [state[0][2], state[1][2], state[2][2]],
+                 [state[0][0], state[1][1], state[2][2]],
+                 [state[0][2], state[1][1], state[2][0]]]
+
+    if ["X", "X", "X"] in win_state:
+        return "X", "Done"
+    elif ["O", "O", "O"] in win_state:
+        return "O", "Done"
+    elif len(empty_cells(state)) == 0:
+        return None, "Draw"
+    else:
+        return None, "Not done"
+
+
+if __name__ == "__main__":
+    playing = True
+    while playing:
+        print("TIC TAC TOE")
+        print("========================================")
+        game_state = [[' ', ' ', ' '],
+                      [' ', ' ', ' '],
+                      [' ', ' ', ' ']]
+        state = "Not done"
+        display_board(game_state)
+        while True:
+            player = input("Please select player to go first (O is you, X is computer): ").upper()
+            if player in players:
+                if player == "X":
+                    player_index = 0
+                else:
+                    player_index = 1
+                break
+            else:
+                print("Please enter a valid input.")
+
+
+
+        while state == "Not done":
+            if player == "O":
+                player_input = input("It's your turn. Please choose a position by entering 2 numbers seperated by a space to place your mark (e.g. 2 1): ")
+                play_move(game_state, player, player_input)
+
+            else:
+                print("It's computer's turn.")
+                position = minimax(game_state, 0, player)
+                play_move(game_state, player, position['move'])
+                print("The computer placed their mark on {}.".format(position['move']))
+
+            display_board(game_state)
+            print("- - - - - - - - - - - - - - - - - - - -")
+
+            winner, state = check_current_state(game_state)
+
+            if winner is not None:
+                if winner == "X":
+                    print("Computer won!")
+                else:
+                    print("You won!")
+            else:
+                player_index = (player_index + 1) % 2
+                player = players[player_index]
+
+            if state == "Draw":
+                print("Draw!")
+
+
+        print("-----------------------------------------")
+        restart = input("Do you want to play again? Y/N: ").upper()
+        if restart == "N":
+            playing = False
