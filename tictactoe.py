daaf1f69998e5656acb592b4d3ac0bd832cca1aa
uyennguyen16900 <uyennguyen16900@gmail.com>
2020-05-04 11:18:50 -0700

    added alpha, beta

--- tictactoe.py
+++ tictactoe.py
@@ -28,20 +28,57 @@ def empty_cells(state):
 
     return cells
 
-def evaluate(state, depth, winner):
+def evaluate(state, winner):
     """Return 1 if computer wins; -1 if human wins; 0 if draw"""
     if state == "Done" and winner == "X":
-        return 10 - depth
+        return 1
     elif state == "Done" and winner == "O":
-        return -10 + depth
+        return -1
     elif state == "Draw":
         return 0
 
-def minimax(state, depth, player):
+# def minimax(state, player):
+#     """"""
+#     winner, current_state = check_current_state(state)
+#     if current_state == "Draw" or current_state == "Done":
+#         return {"score": evaluate(current_state, winner), "move": "-1 -1"}
+#
+#     best = {}
+#
+#     if player == 'X':
+#         best["score"] = -math.inf
+#         best['move'] = ""
+#     else:
+#         best["score"] = math.inf
+#         best['move'] = ""
+#
+#     for cell in empty_cells(state):
+#         x, y = cell[0], cell[1]
+#         state[x][y] = player
+#         if player == "O":
+#             result = minimax(state, "X")
+#         else:
+#             result = minimax(state, "O")
+#         state[x][y] = " "
+#         result['move'] = "{} {}".format(x, y)
+#
+#         # if player is computer
+#         if player == 'X':
+#             if result['score'] > best['score']:
+#                 best = result
+#         # if player is human
+#         else:
+#             if result['score'] < best['score']:
+#                 best = result
+#
+#     return best
+
+
+def minimax(state, player, alpha=-math.inf, beta=math.inf):
     """"""
     winner, current_state = check_current_state(state)
     if current_state == "Draw" or current_state == "Done":
-        return {"score": evaluate(current_state, depth, winner), "move": "-1 -1"}
+        return {"score": evaluate(current_state, winner), "move": "-1 -1"}
 
     best = {}
 
@@ -52,15 +89,13 @@ def minimax(state, depth, player):
         best["score"] = math.inf
         best['move'] = ""
 
-    depth =+ 1
-
     for cell in empty_cells(state):
         x, y = cell[0], cell[1]
         state[x][y] = player
         if player == "O":
-            result = minimax(state, depth, "X")
+            result = minimax(state, "X", alpha, beta)
         else:
-            result = minimax(state, depth, "O")
+            result = minimax(state, "O", alpha, beta)
         state[x][y] = " "
         result['move'] = "{} {}".format(x, y)
 
@@ -68,12 +103,18 @@ def minimax(state, depth, player):
         if player == 'X':
             if result['score'] > best['score']:
                 best = result
+                alpha = max(best['score'], alpha)
         # if player is human
         else:
             if result['score'] < best['score']:
                 best = result
+                beta = min(best['score'], beta)
+
+        if alpha >= beta:
+            break
 
     return best
+
 
 
 def display_board(state):
@@ -81,9 +122,9 @@ def display_board(state):
     print("        0     1     2  ")
     print("     -------------------")
     print("  0  |  {}  |  {}  |  {}  |".format(state[0][0], state[0][1], state[0][2]))
-    print("     -------------------")
+    print("     -------+------+----")
     print("  1  |  {}  |  {}  |  {}  |".format(state[1][0], state[1][1], state[1][2]))
-    print("     -------------------")
+    print("     -------+------+----")
     print("  2  |  {}  |  {}  |  {}  |".format(state[2][0], state[2][1], state[2][2]))
     print("     -------------------")
 
@@ -111,6 +152,8 @@ def check_current_state(state):
 
 if __name__ == "__main__":
     playing = True
+    # alpha = -math.inf
+    # beta = math.inf
     while playing:
         print("TIC TAC TOE")
         print("========================================")
@@ -130,16 +173,14 @@ if __name__ == "__main__":
             else:
                 print("Please enter a valid input.")
 
-
-
         while state == "Not done":
             if player == "O":
-                player_input = input("It's your turn. Please choose a position by entering 2 numbers seperated by a space to place your mark (e.g. 2 1): ")
+                player_input = input("It's your turn. Please choose a position by entering 2 numbers seperated by a space to place your mark (e.g. row column - 2 1): ")
                 play_move(game_state, player, player_input)
 
             else:
                 print("It's computer's turn.")
-                position = minimax(game_state, 0, player)
+                position = minimax(game_state, player)
                 play_move(game_state, player, position['move'])
                 print("The computer placed their mark on {}.".format(position['move']))
 
